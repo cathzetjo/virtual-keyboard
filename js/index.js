ff2c703671506b12ef9245c9be46249be0946ebd
Katerina Zhadko <katerinkaj@icloud.com>
2023-05-01 22:46:42 +0300

    feat: add main structure of app

---
+++ js/index.js
@@ -0,0 +1,28 @@
+const body = document.querySelector('body');
+
+function createElement(tagName, className, innerText) {
+    const element = document.createElement(tagName);
+    if (className) {
+        element.className = className;
+    }
+    if (innerText) {
+        element.innerHTML = innerText;
+    }
+    return element;
+}
+
+function addElement(parent, child) {
+    parent.append(child);
+}
+
+const wrapper = createElement("div", "wrapper");
+addElement(body, wrapper);
+
+const title = createElement("h1", "title", "Virtual Keyboard");
+addElement(wrapper, title);
+
+const textArea = createElement("textarea", "textarea");
+addElement(wrapper, textArea);
+
+const keyboardContainer = createElement("section", "keyboard-container");
+addElement(wrapper, keyboardContainer);
